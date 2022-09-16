5f87283d35d11355310a3c54c4cd488788144bd1
francesco_bertolini <fraberto001@gmail.com>
2022-09-16 12:36:02 +0200

    commit 2 testing comandi generali

---
+++ script.js
@@ -0,0 +1,26 @@
+window.console.log ("ho fatto il cazzo di caffè >:)");
+document.write ("Hello World!");
+var culone = 0;  /* VAR ORMAI SUPERATO COME VARIABILE */
+let tettone = 0; //DA USARE SEMPRE LET
+const capezzoloni = 1; //CONST NECESSITA SEMPRE UN VALORE
+let ano = 1;
+let retto = 1.345;
+let sfintere = "pino";
+let appendice = true;
+
+if (appendice = true) {
+    sfintere = "ano tonante";
+} else {
+    ano =1;
+}
+document.write(sfintere);
+
+for(let i=0; i< 100; i++) {
+    ano++;
+}
+
+while(ano < 101) {
+    ano++;
+    retto++;
+}
+ 
